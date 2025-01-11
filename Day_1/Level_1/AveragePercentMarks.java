d04cf6e60c2847e2a4b29e42c7541039aed98029
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-11 16:46:48 +0530

    program for AveragePercent Marks

---
+++ Day_1/Level_1/AveragePercentMarks.java
@@ -0,0 +1,13 @@
+class AveragePercentMarks{
+  public static void main(String[]args){
+   
+	  //enter the maths,physics and chemistry mark 
+    int maths=94, physics=95, chemistry=96;
+	
+	// finding the average percent mark of sam
+	int averagePercentMark=(94+95+96)*100/300;
+	System.out.println("Sam's average mark in PCM is "+averagePercentMark);
+	
+	
+  }  
+}
