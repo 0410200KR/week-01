821e8364144b8b05a044d7f5a4059b93fc2ba194
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-11 16:50:19 +0530

    program for display result

---
+++ Day_1/Level_1/DisplayResult.java
@@ -0,0 +1,21 @@
+public class DisplayResult
+{
+  public static void main(String[]args)
+  {
+	// enter the name of student
+   String name="sam";
+   
+   // enter the roll number of sam
+    int rollNumber=1;
+	
+   // enter the percent marks of sam
+   double percentMarks=99.99;
+   
+   // enter the result
+   char result='p';
+   
+   // Display the result
+   System.out.println("Displaying Result:\n"+name+"with roll Number: "+rollNumber+" has scored "+percentMarks+"% Marks and Result is " +result);
+   
+  }
+ }
