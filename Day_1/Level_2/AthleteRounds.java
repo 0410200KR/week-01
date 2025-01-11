f5fc624fa446f76ebd131ee5f9feb8804c113ad7
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-11 16:41:53 +0530

    Program of Athlete Round

---
+++ Day_1/Level_2/AthleteRounds.java
@@ -0,0 +1,29 @@
+/*An athlete runs in a triangular park with sides provided as input by the user in meters. If the athlete wants to complete a 5 km run, then how many rounds must the athlete complete
+Hint => The perimeter of a triangle is the addition of all sides and rounds is distance/perimeter
+I/P => side1, side2, side3
+O/P => The total number of rounds the athlete will run is ___ to complete 5 km*/
+import java.util.*;
+
+public class AthleteRounds {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the length of side1 in meters: ");
+        double side1 = input.nextDouble();
+		
+        System.out.print("Enter the length of side2 in meters: ");
+        double side2 = input.nextDouble();
+		
+        System.out.print("Enter the length of side3 in meters: ");
+        double side3 = input.nextDouble();
+         
+		 //calcuate the perimeter of triangle
+        double perimeter = side1 + side2 + side3;
+        double rounds = 5000 / perimeter;
+        
+		//result 
+        System.out.println("The total number of rounds the athlete will run is " + rounds + " to complete 5 km.");
+
+        
+    }
+}
