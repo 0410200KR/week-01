e09116badce0b5c3735529dfabc1bcadafc21cb5
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-11 16:54:25 +0530

    program for harry age

---
+++ Day_1/Level_1/HarryAge.java
@@ -0,0 +1,13 @@
+public class HarryAge{
+ public static void main(String[]args)
+ {
+    birthYear of HarryAge
+   int birthYear=2000;
+   // enter the currentYear of HarryAge
+   int currentYear=2024;
+   // finding actual age of HarryAge
+   int age=currentYear-birthYear;
+   // Display the result
+   System.out.println("Harry's age in 2024 is: "+age+" years");
+  } 
+}
