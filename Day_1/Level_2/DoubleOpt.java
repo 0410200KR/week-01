1730d0ff0e169d588d9db0489e2ff739a77f4ec3
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-12 11:11:10 +0530

    program for double operators

---
+++ Day_1/Level_2/DoubleOpt.java
@@ -0,0 +1,27 @@
+/*Similarly, write the DoubleOpt program by taking double values and doing the same operations.*/
+import java.util.*;
+
+public class DoubleOpt {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the value for a: ");
+        double a = input.nextDouble();
+		
+        System.out.print("Enter the value for b: ");
+        double b = input.nextDouble();
+		
+        System.out.print("Enter the value for c: ");
+        double c = input.nextDouble();
+        
+		//compute the operations
+        double result1 = a + b * c;
+        double result2 = a * b + c;
+        double result3 = c + a / b;
+        double result4 = a % b + c;
+
+        System.out.println("The results of Double Operations are " + result1 + ", " + result2 + ", " + result3 + ", and " + result4);
+
+        
+    }
+}
