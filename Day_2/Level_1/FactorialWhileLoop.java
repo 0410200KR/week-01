5497fe31ca84f10c5a422f840c4dbf37ceb9fe46
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-12 12:07:23 +0530

    program by while loop of factorial

---
+++ Day_2/Level_1/FactorialWhileLoop.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class FactorialWhileLoop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int num = input.nextInt();
+
+        if (num < 0) {
+		
+            System.out.println("Please enter a positive integer.");
+        } 
+		else {
+            int factorial = 1;
+            int i = 1;
+            while (i <= num) {
+                factorial *= i;
+                i++;
+            }
+            System.out.println("Factorial of " + num + " is " + factorial);
+        }
+        
+    }
+}
