4a9f9d77f009bade977aa9b02bafce33e2e161be
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-12 11:16:17 +0530

    program to calculate to simple interest

---
+++ Day_1/Level_2/SimpleInterestCalculator.java
@@ -0,0 +1,29 @@
+/*Write a program to input the Principal, Rate, and Time values and calculate Simple Interest.
+Hint => Simple Interest = Principal * Rate * Time / 100
+I/P => principal, rate, time
+O/P => The Simple Interest is ___ for Principal ___, Rate of Interest ___ and Time ___
+*/
+import java.util.*;
+
+public class SimpleInterestCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the Principal amount: ");
+        double principal = input.nextDouble();
+		
+        System.out.print("Enter the Rate of Interest: ");
+        double rate = input.nextDouble();
+		
+        System.out.print("Enter the Time in years: ");
+        double time = input.nextDouble();
+		
+		
+        // formula Used
+        double simpleInterest = (principal * rate * time) / 100;
+
+        System.out.println("The Simple Interest is " + simpleInterest + " for Principal " + principal + ", Rate of Interest " + rate + " and Time " + time);
+
+        
+    }
+}
