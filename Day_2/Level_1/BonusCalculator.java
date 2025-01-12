ec84901a6d52110391577ec26b90b79efaecd55f
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-12 10:59:03 +0530

    program of Bonus calculation

---
+++ Day_2/Level_1/BonusCalculator.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class BonusCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+		
+        System.out.print("Enter the salary: ");
+        double salary = input.nextDouble();
+		
+        System.out.print("Enter the number of years of service: ");
+        int yearsOfService = input.nextInt();
+
+        if (yearsOfService > 5) {
+            double bonus = salary * 0.05;
+            System.out.println("Bonus amount: " + bonus);
+        } 
+		else {
+            System.out.println("No bonus is awarded.");
+        }
+        
+    }
+}
