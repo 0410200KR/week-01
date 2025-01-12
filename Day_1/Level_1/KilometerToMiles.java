3b273aa22d91ab92a711dfe8286099ca54970f45
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-12 10:46:57 +0530

    a program to convert kilometer to miles

---
+++ Day_1/Level_1/KilometerToMiles.java
@@ -0,0 +1,12 @@
+public class KilometerToMiles
+{
+  public static void main(String[]args)
+  {
+  // enter the given kilometer
+    double kilometers = 10.8;
+	// finding distance in miles
+    double miles = kilometers / 1.6;
+    System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    
+  }
+}
