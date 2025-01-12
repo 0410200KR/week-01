82607c0d529c7ab11cba8be8e889b3fa277955b8
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-12 11:15:06 +0530

    program for Quotient and remainder

---
+++ Day_1/Level_2/QuotientRemainder.java
@@ -0,0 +1,28 @@
+/*Write a program to take 2 numbers and print their quotient and reminder
+Hint => Use division operator (/) for quotient and moduli operator (%) for reminder
+I/P => number1, number2
+O/P => The Quotient is ___ and Reminder is ___ of two number ___ and ___
+*/
+import java.util.*;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int number1 = input.nextInt();
+		
+        System.out.print("Enter the second number: ");
+        int number2 = input.nextInt();
+        
+		//computing quotient and remainder
+        int quotient = number1 / number2;
+        int remainder = number1 % number2;
+		
+        //display result
+        System.out.println("The Quotient is " + quotient + " and Remainder is " + remainder + " of two numbers " + number1 + " and " + number2);
+		
+
+       
+    }
+}
