91957a4dd8a95272c76bfa9d385c4810e4aa41c6
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-12 11:18:56 +0530

    program for calculate total income

---
+++ Day_1/Level_2/TotalIncome.java
@@ -0,0 +1,26 @@
+/*Create a program to find the total income of a person by taking salary and bonus from user
+Hint => 
+Create a variable named salary and take user input.
+Create another variable bonus and take user input.
+Compute income by adding salary and bonus and print the result
+I/P => salary, bonus
+O/P => The salary is INR ___ and bonus is INR ___. Hence Total Income is INR ___*/
+import java.util.*;
+
+public class TotalIncome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the salary: ");
+        double salary = input.nextDouble();
+        System.out.print("Enter the bonus: ");
+        double bonus = input.nextDouble();
+         
+		 //calculating income
+        double totalIncome = salary + bonus;
+        
+		//compute result
+        System.out.println("The salary is INR " + salary + " and bonus is INR " + bonus + ". Hence Total Income is INR " + totalIncome);
+
+    }
+}
