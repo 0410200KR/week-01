600328c057e1fd8e71c1537e2eff7e85954c0b1d
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-12 23:30:25 +0530

    program for finding factors of numbers

---
+++ Day_2/Level_2/FactorsFinder.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class FactorsFinder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+		
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();  
+
+        System.out.println("Factors of " + number + " are:");
+        // Loop from 1 to number
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.println(i);  
+            }
+        }
+       
+    }
+}
