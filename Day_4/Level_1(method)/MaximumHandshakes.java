87deb8ef2829c07735b15bea4d226885d46ba2d6
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-13 10:26:14 +0530

    program to calculate total number of handshakes

---
+++ Day_4/Level_1(method)/MaximumHandshakes.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+//creating a class for calculating maximum handshakes 
+public class MaximumHandshakes {
+
+    public static void main(String[] args) {
+     // making input as an object of Scanner class
+        Scanner input = new Scanner(System.in);
+		
+        System.out.print("Enter number of students: ");
+        int students = input.nextInt();
+        
+        int handshakes = calculateHandshakes(students);
+        System.out.println("Maximum number of handshakes: " + handshakes);
+    }
+
+    public static int calculateHandshakes(int n) {
+        return (n * (n - 1)) / 2;
+    }
+}
